fdd491fc158d2e8eeb8e8e37f6c5577526dd0d89
CLMGI <Popinha76+>
2022-01-19 21:02:45 -0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,7 @@
+import logging
+
+from botocore.exceptions import ClientError
+
+from main import s3_client
+
+
---
+++ main.py
@@ -0,0 +1,57 @@
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Iniciando aplicação...')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+def upload_object(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = file_name
+
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+    return True
+
+
+def create_bucket(bucket_name):
+    try:
+        s3_client.create_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+    return True
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id='AKIAQRJVRKK5N5KSYF5D',
+    aws_secret_access_key='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'
+)
+
+#create_bucket('cjmm-primeirobucket2')
+
+upload_object('teste.txt', 'cjmm-primeirobucket2')
+
+response = s3_client.list_buckets()
+
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
+
+# Resultado:
+# Existing buckets:
+#   cjmm-primeirobucket
